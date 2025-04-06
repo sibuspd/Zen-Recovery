3a24a5feab4e094a6ca79f57129f70649cb89afa
sibuspd <sibuspd@gmail.com>
2025-04-06 16:20:52 +0530

    Initial project push phase

---
+++ app/ui/font.ts
@@ -0,0 +1,19 @@
+import { Inter, Rubik_Dirt, Lusitana  } from "next/font/google"
+
+export const inter = Inter(
+    {
+        subsets: ['latin']
+    }
+); 
+
+export const rubikDirt = Rubik_Dirt(
+    {
+    weight: ["400"]
+    }
+);
+
+export const lusitana = Lusitana(
+    {
+        weight: ['400', '700'],
+        subsets: ['latin'],
+});
