3146ae8b269284e6fcba47a5fe33e1921a076a79
sibuspd <sibuspd@gmail.com>
2025-04-12 18:21:22 +0530

    Added Invoice Add/Edit feature

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,14 +11,13 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import { CardsSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton/>}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
